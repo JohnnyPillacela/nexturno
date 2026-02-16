9ca3965580871b18a94ae4797eefcc586790b703
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-02-16 13:14:44 -0500

    Add undo type

--- lib/events/types.ts
+++ lib/events/types.ts
@@ -2,6 +2,6 @@
 
 export type Event =
   | { type: 'DECLARE_WINNER'; winnerTeamId: string }
-  | { type: 'DECLARE_TIE' };
+  | { type: 'DECLARE_TIE' }
   // | { type: 'RESOLVE_TIE_STAY'; staysTeamId: string }  // M5b
-  // | { type: 'UNDO' }  // M6
+  | { type: 'UNDO' };
